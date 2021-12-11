688975bbbe4783723b93d4e9d5eb0cc5c4b8b592
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-11 13:43:47 +0200

    Update 05. Journey.js

---
+++ 03. Nested-Conditional-Statements/Exercise/05. Journey.js
@@ -0,0 +1,34 @@
+function demo(input) {
+
+    let budget = Number(input[0]);
+    let season = input[1];
+
+    let price = 0;
+
+    if (budget <= 100) {
+        if (season === 'summer') {
+            price = budget * 0.3;
+            console.log('Somewhere in Bulgaria');
+            console.log(`Camp - ${price.toFixed(2)}`);
+        } else {
+            price = budget * 0.7;
+            console.log('Somewhere in Bulgaria');
+            console.log(`Hotel - ${price.toFixed(2)}`);
+        }
+    } else if (budget <= 1000) {
+        if (season === 'summer') {
+            price = budget * 0.4;
+            console.log('Somewhere in Balkans');
+            console.log(`Camp - ${price.toFixed(2)}`);
+        } else {
+            price = budget * 0.8;
+            console.log('Somewhere in Balkans');
+            console.log(`Hotel - ${price.toFixed(2)}`);
+        }
+    } else if (budget > 1000) {
+        price = budget * 0.9;
+        console.log('Somewhere in Europe');
+        console.log(`Hotel - ${price.toFixed(2)}`);
+    }
+}
+demo(["50", "summer"]);
