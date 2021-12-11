66dd47d85877b1f580c31ad0fef32352412d59eb
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-11 11:44:29 +0200

    Create 03. Deposit Calculator.js

---
+++ 01. First-Steps-in-Coding/Exercise/03. Deposit Calculator.js
@@ -0,0 +1,10 @@
+function demo(input) {
+
+    let deposit = Number(input[0]);
+    let deadline = Number(input[1]);
+    let rate = Number(input[2]);
+
+    let total = deposit + deadline * ((deposit * (rate / 100)) / 12);
+
+    console.log(total.toFixed(2));
+}
