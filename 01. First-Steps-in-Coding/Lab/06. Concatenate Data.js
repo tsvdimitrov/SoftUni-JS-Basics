9621d75b8d74ebc3cb781b880a2b89f34e4c84ab
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-11 11:30:28 +0200

    Create 06. Concatenate Data.js

---
+++ 01. First-Steps-in-Coding/Lab/06. Concatenate Data.js
@@ -0,0 +1,9 @@
+function demo(input) {
+
+    let name = input[0];
+    let familyName = input[1];
+    let age = Number(input[2]);
+    let homeTown = input[3];
+
+    console.log(`You are ${name} ${familyName}, a ${age}-years old person from ${homeTown}.`);
+}
