abdc20b960691e58691086dfc0b9a6fc50c62fed
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-11 11:45:28 +0200

    Update 04. Vacation Books List.js

---
+++ 01. First-Steps-in-Coding/Exercise/04. Vacation Books List.js
@@ -0,0 +1,8 @@
+function demo(input) {
+
+    let pages = Number(input[0]);
+    let pagesPerHour = Number(input[1]);
+    let days = Number(input[2]);
+
+    console.log((pages / pagesPerHour) / days);
+}
