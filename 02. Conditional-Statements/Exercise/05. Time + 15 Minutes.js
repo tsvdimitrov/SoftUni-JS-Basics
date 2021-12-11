fde23bac15712670d1b227c481e3a1c7f4fdaa98
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-11 12:15:31 +0200

    Create 05. Time + 15 Minutes.js

---
+++ 02. Conditional-Statements/Exercise/05. Time + 15 Minutes.js
@@ -0,0 +1,21 @@
+function demo(input) {
+
+    let hours = Number(input[0]);
+    let minutes = Number(input[1]) + 15;
+
+    if (minutes >= 60) {
+        hours = hours + 1;
+        minutes = minutes - 60;
+    }
+
+    if (hours >= 24) {
+        hours = hours - 24;
+    }
+    if (minutes > 9) {
+        console.log(`${hours}:${minutes}`);
+    } else {
+        console.log(`${hours}:0${minutes}`);
+    }
+}
+
+demo(["1", "46"]);
