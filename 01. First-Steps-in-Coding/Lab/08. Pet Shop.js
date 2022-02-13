7c672d25cf593ec23f23794f96ab68821da62115
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-02-13 09:21:40 +0200

    Update 08. Pet Shop.js

--- 01. First-Steps-in-Coding/Lab/08. Pet Shop.js
+++ 01. First-Steps-in-Coding/Lab/08. Pet Shop.js
@@ -2,8 +2,8 @@ function demo(input) {
 
     let dogNumber = Number(input[0]);
     let restAnimalsNumber = Number(input[1]);
-dsadsa
+
     let foodCost = (dogNumber * 2.5) + (restAnimalsNumber * 4);
 
     console.log(`${foodCost} lv.`);
-}sadsa
+}
